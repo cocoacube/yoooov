86701d51058f394367333891d592b005670160fa
cocoacube <cocoacube@users.noreply.github.com>
2024-12-31 20:25:05 +0800

    test wandering planet
    test wandering planet

---
+++ wanderplanet/code0.js
@@ -0,0 +1,80 @@
+gdjs.MenuCode = {};
+gdjs.MenuCode.localVariables = [];
+gdjs.MenuCode.GDTitleObjects1= [];
+gdjs.MenuCode.GDTitleObjects2= [];
+gdjs.MenuCode.GDStartObjects1= [];
+gdjs.MenuCode.GDStartObjects2= [];
+gdjs.MenuCode.GDBackgroundObjects1= [];
+gdjs.MenuCode.GDBackgroundObjects2= [];
+gdjs.MenuCode.GDUniversseObjects1= [];
+gdjs.MenuCode.GDUniversseObjects2= [];
+gdjs.MenuCode.GDastronautObjects1= [];
+gdjs.MenuCode.GDastronautObjects2= [];
+
+
+gdjs.MenuCode.eventsList0 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("Start"), gdjs.MenuCode.GDStartObjects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.MenuCode.GDStartObjects1.length;i<l;++i) {
+    if ( gdjs.MenuCode.GDStartObjects1[i].IsClicked((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)) ) {
+        isConditionTrue_0 = true;
+        gdjs.MenuCode.GDStartObjects1[k] = gdjs.MenuCode.GDStartObjects1[i];
+        ++k;
+    }
+}
+gdjs.MenuCode.GDStartObjects1.length = k;
+if (isConditionTrue_0) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Game", false);
+}}
+
+}
+
+
+};gdjs.MenuCode.eventsList1 = function(runtimeScene) {
+
+{
+
+
+gdjs.MenuCode.eventsList0(runtimeScene);
+}
+
+
+};
+
+gdjs.MenuCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.MenuCode.GDTitleObjects1.length = 0;
+gdjs.MenuCode.GDTitleObjects2.length = 0;
+gdjs.MenuCode.GDStartObjects1.length = 0;
+gdjs.MenuCode.GDStartObjects2.length = 0;
+gdjs.MenuCode.GDBackgroundObjects1.length = 0;
+gdjs.MenuCode.GDBackgroundObjects2.length = 0;
+gdjs.MenuCode.GDUniversseObjects1.length = 0;
+gdjs.MenuCode.GDUniversseObjects2.length = 0;
+gdjs.MenuCode.GDastronautObjects1.length = 0;
+gdjs.MenuCode.GDastronautObjects2.length = 0;
+
+gdjs.MenuCode.eventsList1(runtimeScene);
+gdjs.MenuCode.GDTitleObjects1.length = 0;
+gdjs.MenuCode.GDTitleObjects2.length = 0;
+gdjs.MenuCode.GDStartObjects1.length = 0;
+gdjs.MenuCode.GDStartObjects2.length = 0;
+gdjs.MenuCode.GDBackgroundObjects1.length = 0;
+gdjs.MenuCode.GDBackgroundObjects2.length = 0;
+gdjs.MenuCode.GDUniversseObjects1.length = 0;
+gdjs.MenuCode.GDUniversseObjects2.length = 0;
+gdjs.MenuCode.GDastronautObjects1.length = 0;
+gdjs.MenuCode.GDastronautObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['MenuCode'] = gdjs.MenuCode;
